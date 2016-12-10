f529314a9a70287875dc8b6bb95744fc5eb102de
tdvance <tdvance@gmail.com>
2016-12-10 11:24:21 -0500

    Added score displays

---
+++ Assets/_Scenes/Game.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+    public ScoreDisplay score;
+    public ScoreDisplay highScore;
+    public string HighScoreKey = "High Score";
+    public string LevelName = "The Beginning";
+
+    string key;
+    // Use this for initialization
+    void Start() {
+        score.prefixText = "Score: ";
+        highScore.prefixText = "High Score: ";
+        key = "Level[" + LevelName + "].(" + HighScoreKey + ")";
+        LoadHighScore(key);
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void LoadHighScore(string key) {
+        highScore.score = PlayerPrefs.GetInt(key);
+    }
+
+    public void SaveHighScore(string key) {
+        if (score.score > highScore.score) {
+            highScore.score = score.score;
+            PlayerPrefs.SetInt(key, highScore.score);
+        }
+    }
+}
